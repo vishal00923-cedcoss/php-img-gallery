9c3ce0f1e0c252a5f4f29e1ba534d131d9f2fb5b
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-16 18:06:23 +0530

    image gallery

--- src/index.php
+++ src/index.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION["img"])) {
+        $_SESSION["img"] = array();
+    }
+
+    if (move_uploaded_file($_FILES["picture"]["tmp_name"], "uploads/" . $_FILES["picture"]["name"])) {
+        // echo "File is uploaded";
+        array_push($_SESSION["img"], $_FILES["picture"]["name"]);
+    } else {
+        echo "Sorry, there was an error";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +25,15 @@
 <body>
     <h1 class="title">Image Gallery</h1>
     <p>This page displays the list of uploaded images.</p>
+    <form action="" method="post" enctype="multipart/form-data">
+      <input type="file" name="picture" />
+      <input type="submit" name="submit" value="Upload More" class="btn" />
+    </form>
+
+    <?php
+        foreach($_SESSION["img"] as $key => $val) {
+            echo "<img src='uploads/$val' />";
+        } 
+    ?>
 </body>
 </html>
